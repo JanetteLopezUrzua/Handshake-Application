75cfcfe1af36e969922eac7a5e0cff2df17e2e26
Janette <janettelopezurzua@gmail.com>
2020-03-03 23:21:06 -0800

    Added css to the nav bar.

--- Frontend/src/components/Navigationbar.js
+++ Frontend/src/components/Navigationbar.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import { Link } from 'react-router-dom';
 import cookie from 'react-cookies';
+import { FaSearch } from 'react-icons/fa';
 // import {Redirect} from 'react-router';
 import hslogo from '../assets/logo.JPG';
 
@@ -12,6 +17,11 @@ class Navigationbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  componentDidMount() {
+
+
   }
 
    // handle logout to destroy the cookie
@@ -25,11 +35,31 @@ class Navigationbar extends React.Component {
          <Nav.Link>
            <img id="logo" src={hslogo} alt="handshake logo" />
          </Nav.Link>
+         <InputGroup>
+           <InputGroup.Prepend>
+             <InputGroup.Text className="searchbar">
+               <FaSearch />
+             </InputGroup.Text>
+           </InputGroup.Prepend>
+           <Form.Control
+             className="searchbar"
+             id="searchtext"
+             type="text"
+             placeholder="Search"
+           />
+         </InputGroup>
          <Nav className="ml-auto">
-           <Nav.Link href="#home">Jobs</Nav.Link>
-           <Nav.Link href="#link">Events</Nav.Link>
-           <Nav.Link href="#link">Students</Nav.Link>
-           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+           <Nav.Link className="navbaritem" href="#home"><span>Jobs</span></Nav.Link>
+           <Nav.Link className="navbaritem" href="#link"><span>Events</span></Nav.Link>
+           <Nav.Link className="navbaritem" href="#link"><span>Q&amp;A</span></Nav.Link>
+           <Nav.Link className="navbaritem" href="#link"><span>Students</span></Nav.Link>
+           <Nav.Link className="navbaritem" href="#link"><span>Messages</span></Nav.Link>
+           <Nav.Link className="navbaritem" href="#link"><span>Career Center</span></Nav.Link>
+           <NavDropdown
+             className="navbaritem"
+             title=""
+             id="basic-nav-dropdown"
+           >
              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
              <NavDropdown.Item href="#action/3.2">Applications</NavDropdown.Item>
              <NavDropdown.Divider />
